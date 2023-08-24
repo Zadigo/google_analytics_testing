0971dea4b674f84974a3626da1758ae6fb800607
Zadigo <pendenquejohn@gmail.com>
2023-08-24 13:21:41 +0200

    Global improvements

--- google_analytics_testing/urls.py
+++ google_analytics_testing/urls.py
@@ -5,6 +5,8 @@ from google_analytics_testing import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^purchase/success$', views.purchase_success_view, name='purchase_sucess'),
+    re_path(r'^purchase$', views.purchase_view, name='purchase'),
     re_path(r'^subscribe$', views.newsletter_view, name='subscribe'),
     re_path(r'^articles/(?P<pk>\d+)$', views.ArticleView.as_view(), name='article'),
     re_path(r'^articles$', views.ArticlesView.as_view(), name='articles'),
--- google_analytics_testing/views.py
+++ google_analytics_testing/views.py
@@ -1,11 +1,13 @@
 import json
 from functools import lru_cache
+import secrets
 
 from django.shortcuts import redirect, render
+from django.template import Context
 from django.urls import reverse
-from django.views.decorators.http import require_POST
+from django.views.decorators.http import require_POST, require_GET
 from django.views.generic import TemplateView
-
+from django.views.decorators.debug import sensitive_post_parameters
 from google_analytics_testing import forms
 
 
@@ -51,3 +53,34 @@ def newsletter_view(request, **kwargs):
     if form.is_valid():
         pass
     return redirect(reverse('home'))
+
+
+@require_POST
+@sensitive_post_parameters()
+def purchase_view(request, **kwargs):
+    return redirect(reverse('purchase_sucess'))
+
+
+@require_GET
+def purchase_success_view(request, **kwargs):
+    context = Context()
+    context.push({
+        'transaction_id': secrets.token_hex(15),
+        'value': 15,
+        'tax': 4.5,
+        'currency': 'EUR',
+        'items': [
+            {
+                'item_id': 1,
+                'item_name': 'Swimsuit',
+                'index': 1,
+                'item_brand': 'My Brand',
+                'price': 15,
+                'quantity': 1
+            }
+        ]
+    })
+    result = context.flatten()
+    result = json.dumps(result)
+    result = {'purchase': result}
+    return render(request, 'purchase_success.html', result)
