eacd3529afe765d8a2fbf0b6f24dc6f32de7748c
Zadigo <pendenquejohn@gmail.com>
2023-08-24 17:39:58 +0200

    Improvements

--- google_analytics_testing/urls.py
+++ google_analytics_testing/urls.py
@@ -4,12 +4,16 @@ from django.views.generic import TemplateView
 from google_analytics_testing import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    re_path(r'^purchase/success$', views.purchase_success_view, name='purchase_sucess'),
-    re_path(r'^purchase$', views.purchase_view, name='purchase'),
+    re_path(r'^checkout/success$', views.purchase_success_view, name='payment_sucess'),
+    re_path(r'^checkout/payment$', views.payment_view, name='payment'),
+    re_path(r'^checkout/shipping$', views.shipping_view, name='shipping'),
+    re_path(r'^cart/add$', views.cart_add_view, name='add_to_cart'),
+    re_path(r'^cart$', views.cart_view, name='cart'),
+    re_path(r'^purchase$', views.purchase_view, name='start_payment'),
     re_path(r'^subscribe$', views.newsletter_view, name='subscribe'),
     re_path(r'^articles/(?P<pk>\d+)$', views.ArticleView.as_view(), name='article'),
     re_path(r'^articles$', views.ArticlesView.as_view(), name='articles'),
+    path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
 
--- google_analytics_testing/views.py
+++ google_analytics_testing/views.py
@@ -1,6 +1,8 @@
 import json
 from functools import lru_cache
+import random
 import secrets
+from django.http import JsonResponse
 
 from django.shortcuts import redirect, render
 from django.template import Context
@@ -9,6 +11,10 @@ from django.views.decorators.http import require_POST, require_GET
 from django.views.generic import TemplateView
 from django.views.decorators.debug import sensitive_post_parameters
 from google_analytics_testing import forms
+
+
+def transform_dict(data):
+    return json.dumps(data)
 
 
 @lru_cache(maxsize=100)
@@ -56,19 +62,75 @@ def newsletter_view(request, **kwargs):
 
 
 @require_POST
+def cart_add_view(request, **kwargs):
+    return JsonResponse({'state': True})
+
+
+# Ecommmerce process > Cart > checkout : shipping -> payment > Success
+
+@require_GET
+def cart_view(request, **kwargs):
+    context = {
+        'value': random.randrange(20, 2600),
+        'items': [
+            {
+                'item_id': random.randrange(0, 20),
+                'item_name': 'Stan and Friends Tee',
+                'price': 35,
+                'quantity': random.randrange(1, 5)
+            }
+        ]
+    }
+    return render(request, 'cart.html', {'cart': transform_dict(context)})
+
+
+@require_GET
+def shipping_view(request, **kwargs):
+    context = {
+        'value': random.randrange(20, 2600),
+        'items': [
+            {
+                'item_id': random.randrange(0, 20),
+                'item_name': 'Stan and Friends Tee',
+                'price': 35,
+                'quantity': random.randrange(1, 5)
+            }
+        ]
+    }
+    return render(request, 'shipping.html', {'cart': transform_dict(context)})
+
+
+@require_GET
+def payment_view(request, **kwargs):
+    context = {
+        'value': random.randrange(20, 2600),
+        'items': [
+            {
+                'item_id': random.randrange(0, 20),
+                'item_name': 'Stan and Friends Tee',
+                'price': 35,
+                'quantity': random.randrange(1, 5)
+            }
+        ]
+    }
+    return render(request, 'payment.html', {'ecommerce': transform_dict(context)})
+
+
+@require_POST
 @sensitive_post_parameters()
 def purchase_view(request, **kwargs):
-    return redirect(reverse('purchase_sucess'))
+    """Processes the payment"""
+    return JsonResponse({'state': True})
 
 
 @require_GET
 def purchase_success_view(request, **kwargs):
     context = Context()
     context.push({
-        'transaction_id': secrets.token_hex(15),
-        'value': 15,
-        'tax': 4.5,
-        'currency': 'EUR',
+        'transaction_id': secrets.token_hex(10),
+        'value': random.randrange(20, 2600),
+        'tax': 4.9,
+        'shipping': 5.99,
         'items': [
             {
                 'item_id': 1,
